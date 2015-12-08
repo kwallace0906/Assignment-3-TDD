b9bf9c50d7243edcda0c015d0fff1115f55330e3
kwallace0906 <keithgwallace@gmail.com>
2015-12-08 13:27:13 -0500

    1st test commit

---
+++ using-System-using-System.Collections.Generic-using-System.Linq-using-System.Threading.Tasks-using-System.Windows.Forms-namespace-WindowsFormsApplication1-static-class-Program/summary/The-main-entry-point-for-the-application./TriangleTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyTriangle;
+
+namespace MyTriangle.Tests
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        [TestMethod]
+        public void HypotenuseSimple()
+        {
+        }
+    }
+}
