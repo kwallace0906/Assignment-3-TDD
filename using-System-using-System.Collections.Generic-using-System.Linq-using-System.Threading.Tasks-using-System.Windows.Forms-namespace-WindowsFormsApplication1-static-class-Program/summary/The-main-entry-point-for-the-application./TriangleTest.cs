630078559a45249be11abea8d95c1911d2260e11
kwallace0906 <keithgwallace@gmail.com>
2015-12-08 13:39:00 -0500

    first failed test...

---
+++ using-System-using-System.Collections.Generic-using-System.Linq-using-System.Threading.Tasks-using-System.Windows.Forms-namespace-WindowsFormsApplication1-static-class-Program/summary/The-main-entry-point-for-the-application./TriangleTest.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyTriangle;
+
+namespace MyTriangle.Tests
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        [TestMethod]
+        public void HypotenuseSimple()
+        {
+            var triangle = new Triangle();
+            double hypot = triangle.Hypotenuse(4, 5);
+            Assert.AreEqual(0, hypot);
+        }
+    }
+}
