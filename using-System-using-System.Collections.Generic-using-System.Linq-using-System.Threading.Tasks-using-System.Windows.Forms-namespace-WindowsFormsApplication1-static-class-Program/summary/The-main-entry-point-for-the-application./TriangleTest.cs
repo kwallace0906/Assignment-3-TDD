72ad9fe3916fa3e24a7eadbe1e40fec3d4339137
kwallace0906 <keithgwallace@gmail.com>
2015-12-08 15:14:06 -0500

    and done

--- using-System-using-System.Collections.Generic-using-System.Linq-using-System.Threading.Tasks-using-System.Windows.Forms-namespace-WindowsFormsApplication1-static-class-Program/summary/The-main-entry-point-for-the-application./TriangleTest.cs
+++ using-System-using-System.Collections.Generic-using-System.Linq-using-System.Threading.Tasks-using-System.Windows.Forms-namespace-WindowsFormsApplication1-static-class-Program/summary/The-main-entry-point-for-the-application./TriangleTest.cs
@@ -1,3 +1,11 @@
+/*
+Software Quality 1 Assignment 3
+Keith Wallace 7080906
+Dec 8th 2015
+The following code is a testing suite for the Triangle class library interface.
+Each unit test was successfully passed showing that the Triangle interface's methods
+are working as expected
+*/
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyTriangle;
@@ -8,7 +16,7 @@ namespace MyTriangle.Tests
     public class TriangleTests
     {
         [TestMethod]
-        public void RightHypotenuseSimple()
+        public void RightHypotenuseSimple() 
         {
             var triangle = new Triangle();
             double hypot = triangle.RightHypotenuse(3, 4);
